f1bbcedbbff053b6618ff5557a6f3dce9a5a5c1f
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2025-03-05 23:16:45 +0530

    gptv push

--- model.py
+++ model.py
@@ -248,3 +248,155 @@ class GPTk(nn.Module):
             if next_tokens[0]==50256: break
         
         return tokens[0].tolist()[input_len:]
+    
+
+
+class GPTv(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.config = config
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            ln_f = nn.LayerNorm(config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)])
+        ))
+        
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+        self.lm_vectors = nn.Embedding(num_embeddings=config.k, embedding_dim=config.n_embd)
+
+        self.apply(self._init_weights)
+
+        for pn, p in self.named_parameters():
+            if pn.endswith('c_proj.weight'):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))            
+
+    def get_num_params(self, non_embedding=True):
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.transformer.wpe.weight.numel()
+        return n_params
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+        
+    def forward(self, idx):
+        device = idx.device
+        b, t = idx.size()
+        assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device) # shape (t)
+
+        tok_emb = self.transformer.wte(idx) 
+        pos_emb = self.transformer.wpe(pos) 
+        x = tok_emb + pos_emb
+        for block in self.transformer.h:
+            x = block(x)
+        
+        logits = {} 
+        for k in range(self.config.k):
+            logits[k] = self.lm_head(
+                self.transformer.ln_f(x + self.lm_vectors(torch.tensor(k, device=x.device)))
+            )
+
+        return logits
+    
+    @classmethod
+    def from_pretrained(cls, model_type="gpt2", k: int=3, r: int = 64):
+
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        print(f"Loading weights from pretrained GPT-2 model: {model_type}")
+
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024),
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280),
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600),
+        }[model_type]
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+
+        model_hf = GPT2LMHeadModel.from_pretrained(f"./{model_type}")
+        model_hf.eval()
+
+        for param in model_hf.parameters():
+            param.requires_grad = False
+        print("Frozen all transformer weights.")
+
+        print("Aligning model weights...")
+        config_args.update({"k": k, "r": r})
+        config = GPTConfig(**config_args)
+        model = cls(config)  
+        sd = model.state_dict()
+        sd_keys = [k for k in sd.keys() if not k.endswith('.attn.bias')]
+
+        sd_hf = model_hf.state_dict()
+        sd_keys_hf = [k for k in sd_hf.keys() if not k.endswith('.attn.masked_bias') and not k.endswith('.attn.bias')]
+
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # assert len(sd_keys_hf) == len(sd_keys), f"Mismatch: {len(sd_keys_hf)} vs {len(sd_keys)}"
+
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        print(f"Modified {model_type} model loaded into GPTk.")
+        return model
+
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        param_dict = {pn: p for pn, p in self.named_parameters() if p.requires_grad}
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and device_type == 'cuda'
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+        print(f"using fused AdamW: {use_fused}")
+
+        return optimizer
+
+    
+    def generate(
+        self,
+        tokens: torch.Tensor,
+        max_new_tokens: int = 512
+    ) -> None:
+        
+        input_len = tokens.shape[0]
+
+        for _ in range(max_new_tokens):
+            logits = self(tokens)
+            next_tokens, next_token_probs = [], []
+            for i in range(self.config.k):
+                next_tokens.append(torch.argmax(logits[i][:, -1, :], dim=-1).squeeze().item())
+                max_val, _ = torch.max(F.softmax(logits[i], dim=-1), dim=-1)
+                next_token_probs.append(max_val[0][0].item())
+            
+            tokens = torch.cat(
+                [tokens, torch.tensor([[next_tokens[0]]], device=tokens.device)], 
+                dim=-1
+            ).to(tokens.device)
+            if next_tokens[0]==50256: break
+        
+        return tokens[0].tolist()[input_len:]
